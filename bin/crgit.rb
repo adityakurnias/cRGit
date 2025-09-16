026eb0b5ddeacbcf125799f8ff6764ea53cf4997
adityakurnias <adityakurniasaputra903@gmail.com>
2025-09-16 22:51:26 +0700

    feat(crgit): Initialize

---
+++ bin/crgit.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# bin/crgit
+
+command, *args = ARGV
+
+if command.nil?
+  warn 'Usage: crgit <command> [<args>]'
+  exit 1
+end
+
+path_to_command = File.expand_path("crgit-#{command}", __dir__)
+unless File.exist?(path_to_command)
+  warn "No such command: #{command}"
+  exit 1
+end
+
+exec path_to_command, *args
