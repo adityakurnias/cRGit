46d9ea5634baaa20b43d37d62678c247730e7e82
adityakurnias <adityakurniasaputra903@gmail.com>
2025-09-16 23:01:14 +0700

    feat(status): Add status

---
+++ bin/crgit-status.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# bin/rgit-status
+
+lib_path = File.expand_path('../lib', __dir__)
+$LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include?(lib_path)
+require 'crgit/index'
+require 'crgit/repo'
+
+include CRGit
+
+unless Dir.exist?(CRGit::RGIT_DIR)
+  warn 'Not an RGit project'
+  exit 1
+end
+
+puts 'Changes to be committed:'
+Index.entries.each do |sha, path|
+  puts "\tnew file:   #{path} (#{sha[0..6]})"
+end
+
+head = Repo.new.head_commit
+puts "\nCurrent HEAD: #{head || '(none)'}"
