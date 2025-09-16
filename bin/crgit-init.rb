89ec1c9f5ab4d7ff1d905c34967012bed2f75b1e
adityakurnias <adityakurniasaputra903@gmail.com>
2025-09-16 22:56:37 +0700

    feat(init): Add init crgit

---
+++ bin/crgit-init.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# bin/crgit-init.rb
+
+lib_path = File.expand_path('../lib', __dir__)
+$LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include?(lib_path)
+require 'fileutils'
+require 'crgit/repo'
+require 'crgit/object'
+
+include CRGit
+
+Repo.new.init
